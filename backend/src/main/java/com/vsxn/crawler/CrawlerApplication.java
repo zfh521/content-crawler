ba37032901704fd67d0d4192f58a32efba731ee7
zhangfanhang <Fan.Hang.Zhang@cn.ey.com>
2021-12-13 18:06:40 +0800

    first commit

---
+++ backend/src/main/java/com/vsxn/crawler/CrawlerApplication.java
@@ -0,0 +1,13 @@
+package com.vsxn.crawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrawlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrawlerApplication.class, args);
+	}
+
+}
